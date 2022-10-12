733479aa51c994d8843230e1f0ae92855fa9ad2b
DanielP <Danielpihl22@gmail.com>
2022-10-12 19:27:18 +0200

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,23 @@
 extern "C"
 {
     #include "taylor_sine.h"
-    #include "stach.h"
 }
 
 
 // Add your test cases for Exercise 1b,c) here
 
 int main(void)
+
 {
     double test[6] = {0.0 , 0.4 , 0.9, 4.2 , 9.0 , 22.0}; 
 
     for (int i = 0; i < 6; i++)
     {
-        printf()
+        printf("testing Taylor Sine function: %lf\n", taylor_sine(test[i], 5));
+        printf("Build in sinus function: %lf\n", sin(test[i])); 
+
     }
 }
+
+// The value "x" is most precise for the "taylor_sine" function the lower the value is compared to build in "sin" function
+// we can get a more precise result if we increase the itterations of "taylor_sine" function. 
