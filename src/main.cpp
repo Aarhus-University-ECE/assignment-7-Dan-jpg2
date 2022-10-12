e0d1a04fbe7ddde81500fc33c3311fe96e92cfe6
DanielP <Danielpihl22@gmail.com>
2022-10-12 19:17:28 +0200

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,22 @@
 #include <stdio.h>
 #include <math.h>
 #include <assert.h>
+
 extern "C"
 {
-#include "taylor_sine.h"
+    #include "taylor_sine.h"
+    #include "stach.h"
 }
 
 
 // Add your test cases for Exercise 1b,c) here
 
-int main(int argc, char **argv){
+int main(void)
+{
+    double test[6] = {0.0 , 0.4 , 0.9, 4.2 , 9.0 , 22.0}; 
 
-    printf("%1f", taylor_sine(0.000,4));
-    return 0; 
+    for (int i = 0; i < 6; i++)
+    {
+        printf()
+    }
 }
