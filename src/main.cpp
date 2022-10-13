874af46cf871e4624dd2ada36276778bb5eba8c2
DanielP <Danielpihl22@gmail.com>
2022-10-13 13:39:47 +0200

    Should be Done

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 extern "C"
 {
     #include "taylor_sine.h"
+    #include "stack.h"
 }
 
 
@@ -14,15 +15,30 @@ extern "C"
 int main(void)
 
 {
-    double test[6] = {0.0 , 0.4 , 0.9, 4.2 , 9.0 , 22.0}; 
+    double test[6] = {0.0 , 0.4 , 0.9, 3.1415 , 9.0 , 15.0}; 
 
     for (int i = 0; i < 6; i++)
     {
         printf("testing Taylor Sine function: %lf\n", taylor_sine(test[i], 5));
-        printf("Build in sinus function: %lf\n", sin(test[i])); 
+        printf("Build in sinus function: %lf \n\n", sin(test[i])); 
 
     }
-}
 
 // The value "x" is most precise for the "taylor_sine" function the lower the value is compared to build in "sin" function
 // we can get a more precise result if we increase the itterations of "taylor_sine" function. 
+// but we will be limited by memory allocation if using this method of calculating sinus.
+
+stack first; 
+initialize(&first); 
+push(5, &first);
+printf("%d\n", pop(&first)); 
+push(4, &first);
+printf("%d\n", pop(&first));
+printf("%d\n", empty(&first)); 
+
+return 0; 
+
+
+
+
+}
--- src/taylor_sine.c
+++ src/taylor_sine.c
@@ -4,27 +4,30 @@
 #include <math.h> 
 
 //function to calculate the factorial of x
-int factorial(int x){
+double factorial(int x){
     if (x==0) 
         return 1;
     else
-        return(x*factorial(x-1)); 
+        return(x * factorial(x - 1)); 
 }
 
 //function for finding sin(x) with Taylor Series up to n'th terms
 double taylor_sine(double x, int n){ //function
-    int y = 3; //initial exponentiation of x^n | y is used as a counter as well
+    int y = 2; //initial exponentiation of x^n | y is used as a counter as well
     double power = 5; //second exponentiation of x^n
     double result = 0.0; //end result of function
 
-    result = x-(pow(x,3)/factorial(3)); // first term of Taylor Series
-    for(y; y<=n; y++); {
+    result = x-(pow(x,3)/factorial(3)); // first two term of Taylor Series
+    for(y; y<=n; y++) 
+    {
         if(y % 2 == 0){ //if there isn't a rest after dividing a sequence / term then the result will be result + 
              result = result + (pow(x,power)/factorial(power)); 
         }
         else
             result = result - (pow(x,power)/factorial(power)); //if there is a rest after dividing a sequence / term then the result will be result -
+        power += 2; 
     }
+
     
     return result;  //the value we want to return to the user
 }
